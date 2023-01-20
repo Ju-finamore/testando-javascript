31c93d44153c4bbf27f33b620ce1dfd92ae4c2da
julia-finamore <juliamm_finamore@hotmail.com>
2023-01-20 10:34:28 -0300

    Adicionei a formula de idade em segundos no arquivo js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("Olá mundo");
+// Perguntar a idade do usuário através do prompt()
+let age = prompt("Qual sua idade?")
+// converter a idade em segundos
+let ageInSeconds = age*365*24*60*60
+console.log(ageInSeconds);
+// mostrar o resultado usando document.write()
+document.write("Em seus "+ age +" anos de idade, se passaram "+ ageInSeconds +" segundos")
